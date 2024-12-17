b1dbc7e311610eb694f8750df81e3c1d0de732f0
FabioLWT <fabiotamazzia15@gmail.com>
2024-12-17 15:23:20 -0300

    Descrição clara das alterações

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ app.use(express.json());
 
 // cria uma rota get para testar a api
 app.get("/health", (request, response) => {
-  response.send("Seja Bem-vindo à minha API Vai corinthais");
+  response.send("Seja Bem-vindo à minha API Vai!! corinthais");
 });
 
 let users = [];
@@ -17,14 +17,32 @@ let users = [];
 app.post("/users", (request, response) => {
   const { name, age } = request.body;
 
-  const newUser = { name, age };
+  const newUser = {
+    id: users.length ? users[users.length - 1].id + 1 : 1,
+    name,
+    age,
+  };
   users.push(newUser);
 
   response.send(newUser);
 });
 
+// cria uma rota get para listar todos os users
 app.get("/users", (request, response) => {
   response.send(users);
+});
+
+// cria uma rota get para listar um user específico
+app.get("/users/:id", (request, response) => {
+  const userId = parseInt(request.params.id, 10); // Converte o id para número
+  console.log(userId); // Para verificar o valor do id
+  const currentUser = users.find((user) => user.id === userId);
+
+  if (currentUser) {
+    response.send(currentUser);
+  } else {
+    response.status(404).send({ error: "Usuário não encontrado" });
+  }
 });
 
 // inicia o servidor na porta 3001
